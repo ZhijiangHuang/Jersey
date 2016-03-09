bce3a74272784399c60fb322664a32616fc8ec64
HUANGZHIJIANG416 <111>
2016-03-09 16:44:10 +0800

    different annotation

--- src/main/java/com/zhi/MyResource.java
+++ src/main/java/com/zhi/MyResource.java
@@ -1,10 +1,9 @@
 package com.zhi;
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by HUANGZHIJIANG416 on 2016/3/8.
@@ -17,5 +16,53 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String ping(){
         return "Hey, This is Jersey JAX-RS!";
+    }
+
+    @GET
+    @Produces("text/plain")
+    @Path("getHello")
+    public String getHello(){
+        return "hello";
+    }
+
+    @Path("/user/{username}")
+    @GET
+    @Produces("text/plain")
+    public String getUser(@PathParam("username") String userName){
+        return "hello " + userName;
+    }
+
+    @PUT
+    @Path("put")
+    @Produces("text/plain")
+    public String putMethod(){
+        return "put done!";
+    }
+
+    @Path("html")
+    @Produces("text/html")
+    @GET
+    public String doGetAsHtml(){
+        return "html";
+    }
+
+    @Path("json")
+    @Produces("application/json")
+    @GET
+    public String doGetAsJson(){
+        return "json";
+    }
+
+    @Path("smooth")
+    @GET
+    @Produces("text/plain")
+    public String smooth(
+            @DefaultValue("2") @QueryParam("step") int step,
+            @DefaultValue("false") @QueryParam("min-m") boolean hasMin,
+            @DefaultValue("false") @QueryParam("max-m") boolean hasMax,
+            @DefaultValue("true") @QueryParam("last-m") boolean hasLast) {
+        boolean b = false;
+        b=true;
+        return step + String.valueOf(hasMin) + String.valueOf(hasMax);
     }
 }
--- src/main/test/MyResourceTest.java
+++ src/main/test/MyResourceTest.java
@@ -1,6 +1,14 @@
+
+
 /**
  * Created by HUANGZHIJIANG416 on 2016/3/8.
  */
 public class MyResourceTest {
+//    private HttpServer server;
+//    private WebTarget target;
+//
+//    public void setUp(){
+//
+//    }
 //    private WebTarget target;
 }
